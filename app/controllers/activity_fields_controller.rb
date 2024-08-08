8d0ecef45b1b13f6622858e72f475f52dd964457
Edivan de Castro <edivandecastro@gmail.com>
2024-08-08 18:20:28 -0300

    Implemented serializer in controller ActivityFieldsController

--- app/controllers/activity_fields_controller.rb
+++ app/controllers/activity_fields_controller.rb
@@ -1,5 +1,6 @@
 class ActivityFieldsController < ApplicationController
   def index
-    @activity_fields = ActivityField.all
+    activity_fields = ActivityField.all
+    render json: Panko::ArraySerializer.new(activity_fields, each_serializer: ActivityFieldSerializer).to_json
   end
 end
