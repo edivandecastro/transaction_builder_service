73a4c33a7f6c163fb77b73e9daa213fca89f0b3d
Edivan de Castro <edivandecastro@gmail.com>
2025-03-16 21:03:55 -0300

    Add TransactionsController for handling transaction operations in v1 namespace

---
+++ app/controllers/v1/transactions_controller.rb
@@ -0,0 +1,32 @@
+module V1
+  class TransactionsController < ApplicationController
+
+    def index
+      transactions = Schema::Transaction.page(params[:page]).per(params[:limit])
+
+      render json: Panko::Response.new(
+        metadata: meta_attributes(transactions),
+        transactions: Panko::ArraySerializer.new(transactions, each_serializer: Schema::TransactionSerializer)
+      )
+    end
+
+    def create
+      transaction = Schema::Transaction.new(transaction_params)
+
+      if transaction.save
+        render_json root: :transaction,
+                    resource: transaction,
+                    serializer: Schema::TransactionSerializer,
+                    status: :created
+      else
+        render json: transaction.errors, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def transaction_params
+      params.require(:transaction).permit(:name)
+    end
+  end
+end
